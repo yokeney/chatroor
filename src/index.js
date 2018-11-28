b4ef7d0e3bc0c3866da222740003b95b93796daf
yokeney <965660471@qq.com>
2018-11-28 10:03:29 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React,{Component} from "react";
-import ReactDOM from 'react-dom';
-import {Button} from 'antd-mobile'
  export default class Aa extends Component{
  constructor(){
      super();
+
      }
      render(){
          return (
-             <div><Button type="primary">222</Button></div>
+             <div>aa</div>
          )
      }
  }
- ReactDOM.render(
-     <Aa />,document.getElementById('root')
- )
