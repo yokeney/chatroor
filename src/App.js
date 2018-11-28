d3a653e9c1c84e26523fc189fbe9870e6ad64873
yokeney <965660471@qq.com>
2018-11-28 14:00:07 +0800

    redux 4.3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React,{Component} from "react";
 import {Button} from 'antd-mobile'
-import {AddGun} from './index_redux'
   class App extends Component{
      render(){
          const store=this.props.store;
          const nmu=store.getState();
+         const AddGun=this.props.AddGun;
+         const DelGun=this.props.DelGun;
          return (
              <div>
                  <h1>现在有{nmu}把</h1>
-                 <Button type="primary" onClick={()=>store.dispatch(AddGun())}></Button>
+                 <Button type="primary" onClick={()=>store.dispatch(AddGun())}>加</Button>
+                 <Button type="danger" onClick={()=>store.dispatch(DelGun())}>减</Button>
              </div>
          )
      }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDom from 'react-dom';
 import App from './App';
 import {createStore} from 'redux';
-import {counter} from './index_redux.js'
+import {counter,AddGun,DelGun} from './index_redux.js'
 const store=createStore(counter)
 function render(){
-    ReactDom.render(<App store={store}/>,document.getElementById('root'))
+    ReactDom.render(<App store={store} AddGun={AddGun} DelGun={DelGun}/>,document.getElementById('root'))
 }
 render();
 store.subscribe(render);
