3d6a503e14a22ec4c63d90d5b6127e785e720eb6
yokeney <965660471@qq.com>
2018-11-28 15:29:32 +0800

    redux 4.5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDom from 'react-dom';
 import App from './App';
-import {applyMiddleware,createStore} from 'redux';
+import {applyMiddleware,createStore,compose} from 'redux';
 import thunk from 'redux-thunk';
 import {counter,AddGun,DelGun,AddGunAsync} from './index_redux.js'
-const store=createStore(counter,applyMiddleware(thunk));
+const store=createStore(counter,
+    compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension()?window.devToolsExtension():f=>f
+    ))
 function render(){
     ReactDom.render(<App store={store} AddGun={AddGun} AddGunAsync={AddGunAsync} DelGun={DelGun} />,document.getElementById('root'))
 }
