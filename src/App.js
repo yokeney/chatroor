af248875c287ced4b92e627da04dc6043183b43b
yokeney <965660471@qq.com>
2018-12-04 11:27:48 +0800

    4.6react-redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React,{Component} from "react";
 import {Button} from 'antd-mobile'
+import {connect} from 'react-redux'
+import {AddGun,DelGun,AddGunAsync} from './index_redux'
   class App extends Component{
      render(){
-         const store=this.props.store;
-         const nmu=store.getState();
-         const AddGun=this.props.AddGun;
-         const DelGun=this.props.DelGun;
-         const AddGunAsync=this.props.AddGunAsync;
          return (
              <div>
-                 <h1>现在有{nmu}把</h1>
-                 <Button type="primary" onClick={()=>store.dispatch(AddGun())}>加</Button>
-                 <Button type="danger" onClick={()=>store.dispatch(DelGun())}>减</Button>
-                 <Button type="danger" onClick={()=>store.dispatch(AddGunAsync())}>减</Button>
+                 <h1>现在有{this.props.num}把</h1>
+                 <Button type="primary" onClick={this.props.AddGun}>加</Button>
+                 <Button type="danger" onClick={this.props.DelGun}>减</Button>
+                 <Button type="danger" onClick={this.props.AddGunAsync}>减</Button>
              </div>
          )
      }
  }
+ const mapStatetoProps=(state)=>{
+     return {num:state}
+ }
+ const actionCreators={AddGun,DelGun,AddGunAsync};
+ App=connect(mapStatetoProps,actionCreators)(App)
  export default App
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDom from 'react-dom';
 import App from './App';
 import {applyMiddleware,createStore,compose} from 'redux';
 import thunk from 'redux-thunk';
-import {counter,AddGun,DelGun,AddGunAsync} from './index_redux.js'
+import {Provider} from 'react-redux';
+import {counter} from './index_redux.js'
 const store=createStore(counter,
     compose(
         applyMiddleware(thunk),
-        window.devToolsExtension()?window.devToolsExtension():f=>f
+        window.devToolsExtension?window.devToolsExtension():f=>f
     ))
-function render(){
-    ReactDom.render(<App store={store} AddGun={AddGun} AddGunAsync={AddGunAsync} DelGun={DelGun} />,document.getElementById('root'))
-}
-render();
-store.subscribe(render);
+    ReactDom.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById('root'))
+// store.subscribe(render);
