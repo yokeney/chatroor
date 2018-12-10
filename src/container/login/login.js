761b5aaae27a2d98e8b50d1ab32ca777d12fe347
yokeney <965660471@qq.com>
2018-12-10 11:40:01 +0800

    6.11 and

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -2,27 +2,49 @@ import React,{Component} from "react";
 import Logo from '../../component/logo/logo.js'
 import {List,InputItem,WingBlank,Button,WhiteSpace} from 'antd-mobile'
 import AuthRoute from '../../component/authroute/authroute.js'
+import {login} from '../../redux/user.redux.js';
+import {Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+@connect(
+    state=>state.user,
+    {login}
+)
   class Login extends Component{
       constructor(){
           super();
           this.register=this.register.bind(this);
+          this.handleLogin=this.handleLogin.bind(this);
+          this.state={
+              user:'',
+              pwd:''
+            }
           }
           register(){
               this.props.history.push('./register')
           }
+          handleChange(key,value){
+              this.setState({
+                  [key]:value
+              })
+          }
+          handleLogin(){
+              this.props.login(this.state);
+          }
      render(){
          return (
             <div>
+            {this.props.redirectTo?<Redirect to={this.props.redirectTo}/>:null}
                 <Logo />
                 <WingBlank>
                     <List>
-                        <InputItem>用户名</InputItem>
+                        {this.props.msg?<p className="error">{this.props.msg}</p>:null}
+                        <InputItem onChange={v=>this.handleChange("user",v)}>用户名</InputItem>
                         <WhiteSpace />
-                        <InputItem>密码</InputItem>
+                        <InputItem onChange={v=>this.handleChange("pwd",v)}>密码</InputItem>
                     </List>
-                    <Button type="primary">登陆</Button>
+                    <Button type="primary" onClick={this.handleLogin}>登陆</Button>
                     <WhiteSpace />
-                    <Button type="primary" onClick={this.register}>注册</Button>
+                    <Button  type="primary" onClick={this.register}>注册</Button>
                 </WingBlank>
             </div>
          )
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import {getRedirectPath} from '../util.js'
+const LOGINSUCCESS="LOGINSUCCESS";
 const REGISTER_SUCCESS="REGISTER_SUCCESS";
 const ERROR_MSG="ERROR_MSG";
 const initState={
@@ -15,6 +16,8 @@ export function user(state=initState,action){
     switch (action.type) {
         case REGISTER_SUCCESS:
             return {...state,isAuth:true,redirectTo:getRedirectPath(action.payload),msg:'',...action.payload}
+        case LOGINSUCCESS:
+            return {...state,isAuth:true,redirectTo:getRedirectPath(action.payload),msg:'',...action.payload}
         case ERROR_MSG:
             return {...state,isAuth:false,msg:action.msg}
         default:
@@ -26,6 +29,24 @@ function RegisterSuccess(data){
 }
 function errorMsg(msg){
     return {type:ERROR_MSG,msg:msg}
+}
+function loginSuccess(data){
+    return {type:LOGINSUCCESS,payload:data}
+}
+export function login({user,pwd}){
+    if (!user||!pwd) {
+        return errorMsg("用户名密码必须输入");
+    }
+    return dispatch=>{
+        axios.post('/user/login',{user,pwd}).then(res=>{
+            if(res.status===200&&res.data.code===0){
+                dispatch(loginSuccess(res.data.data));
+            }
+            else{
+                dispatch(errorMsg(res.data.msg))
+            }
+        })
+    }
 }
 export function register({user,pwd,repwd,type}){
     if(!user||!pwd||!type||!repwd){
